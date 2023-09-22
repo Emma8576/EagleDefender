515fe476afab83ab35fc22f3552aaf3e7c6ac8ca
oreoavt <bryanmongenavarro1@gmail.com>
2023-09-22 17:33:19 -0600

    new configuration welcomeInterface

--- welcomeInterface.py
+++ welcomeInterface.py
@@ -1,53 +1,28 @@
-import pygame
-from pygame import *
-import sys
-import time
+import tkinter as tk
+from PIL import Image, ImageTk
 
-init()
+# Crear la ventana principal
+root = tk.Tk()
+root.title("Reproducción de GIF en bucle")
 
-screen_info = display.Info()
-screen_width = screen_info.current_w
-screen_height = screen_info.current_h
+# Cargar el GIF
+gif_path = "welcomeInterfaceFramesSprites/mainWelcome2/mainWelcome1 (1).gif"
+gif = Image.open(gif_path)
+frame_count = gif.n_frames
+frames = [ImageTk.PhotoImage(gif.copy().seek(i)) for i in range(frame_count)]
 
-screen = display.set_mode((screen_width, screen_height), FULLSCREEN)
+# Mostrar el GIF en un Label
+label = tk.Label(root)
+label.pack()
 
-images = []
-images_left = []
-images_right = []
+# Función para actualizar el GIF
+def update_frame(idx):
+    frame = frames[idx]
+    label.configure(image=frame)
+    root.after(100, update_frame, (idx + 1) % frame_count)
 
+# Iniciar la actualización del GIF
+update_frame(0)
 
-
-#Programar sprites ventana central
-for i in range(1, 14):
-    name = "welcomeInterfaceFramesSprites/mainWelcome/frame-"+str(i)+".gif"
-    images.append(image.load(name))
-
-#Programar sprites lateral izquieda
-for i in range(1,19):
-    name_left = "welcomeInterfaceFramesSprites/secondWelcome/frame-" + str(i) + ".gif"
-    images_left.append(image.load(name_left))
-
-    name_right = "welcomeInterfaceFramesSprites/secondWelcome/frame-" + str(i) + ".gif"
-    images_right.append(image.load(name_right))
-
-def titleImage():
-    picture = pygame.image.load("welcomeInterfaceFramesSprites/mainWelcomeImages/mainTitle.png")
-    picture = pygame.transform.scale(picture, [500,500])
-    screen.blit(picture, [380,50])
-
-while True:
-    for e in event.get():
-        if e.type == QUIT: sys.exit()
-
-    frame = int(time.time()*10)
-    frame %= len(images)
-
-    screen.fill((255, 255, 255))
-
-    screen.blit(images[frame], (280,10))
-    screen.blit(images_left[frame],(-69,10))
-    screen.blit(images_right[frame], (980, 10))
-
-    titleImage()
-
-    display.flip()
+# Iniciar la aplicación
+root.mainloop()
