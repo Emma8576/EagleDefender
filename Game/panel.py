55d40b5330f67acef3faf37cf6f8897638e5ec02
oreoavt <bryanmongenavarro1@gmail.com>
2023-10-07 22:52:27 -0600

    new configuration panel game

--- Game/panel.py
+++ Game/panel.py
@@ -1,23 +1,38 @@
 import pygame
 import sys
+from pygame import *
+import time
+
 
 pygame.init()
+
+screen_info = display.Info()
+screen_width = screen_info.current_w
+screen_height = screen_info.current_h
+screen = display.set_mode((screen_width, screen_height), FULLSCREEN)
 
 
 clock = pygame.time.Clock()
 
-size = (1980,1080)
-screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
+bg = []
 
-background = pygame.image.load("panel_elements/bg/bg (1).png").convert()
+for i in range(1,24):
+	nombre_bg="panel_elements/bg/frame-"+str(i)+".gif"
+	bg.append(image.load(nombre_bg))
+
 
 while True:
-	screen.blit(background,[0,0])
+	screen.fill((255,0,0))
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			sys.exit()
 		if event.type == pygame.KEYDOWN:
 			sys.exit()
+
+	frame = int(time.time()*10)
+	frame %= len(bg)
+
+	screen.blit(bg[frame],(0,0))
 
 	pygame.display.flip()
 	clock.tick(60)
